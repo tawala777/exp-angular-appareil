66924f8b3237854889a9919ed5eb4a82365358ea
Andrew Ackah-Miezan <tawala@gmail.com>
2018-05-12 21:09:42 +0200

    2way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CompoComponent } from './compo/compo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CompoComponent } from './compo/compo.component';
     CompoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
