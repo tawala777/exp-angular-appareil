dfd5c771f42a53c7236faca002a25a4b3c80fb73
Andrew Ackah-Miezan <tawala@gmail.com>
2018-05-13 03:21:00 +0200

    divers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   isAuth = false;
+//  lastUpdate = new Date();
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
   appareilOne = 'Ordinateur';
   appareils = [
     {
