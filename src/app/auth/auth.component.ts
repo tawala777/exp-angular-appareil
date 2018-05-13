dd113ff5d864cb2d5f3d4da983fa61f257f6e336
Andy A <tawala@gmail.com>
2018-05-14 01:01:10 +0200

    test2

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import {Router} from '@angular/router'
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  authStatus: boolean;
+
+  constructor(private authSrv: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+  onSeConnecter(){
+    console.log('on se connecte !');
+    this.authSrv.signIn().then (
+      () => {
+        this.authStatus = this.authSrv.isAuth;
+        console.log('je suis connecté = ',this.authStatus);
+        //this.router.navigate(['appareils']);
+      }
+    )
+
+  }
+  onSeDeconnecter(){
+    console.log('on se deconnecte !');
+    this.authSrv.signOut();
+    this.authStatus = this.authSrv.isAuth;
+    console.log('je suis connecté = ',this.authStatus);
+  }
+}
---
+++ src/app/compo-view/compo-view.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
+
+@Component({
+  selector: 'app-compo-view',
+  templateUrl: './compo-view.component.html',
+  styleUrls: ['./compo-view.component.css']
+})
+
+export class CompoViewComponent implements OnInit {
+  appareils : any[];
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  
+  constructor(private appSrv: AppareilService) { }
+
+
+  ngOnInit() {
+    /* BAD PRACTICE !*/ 
+    this.appareils = this.appSrv.appareils;
+  }
+  onAllumer() {
+    console.log('On allume tout !');
+    this.appSrv.allumerTout();
+  }  
+  onEteindre() {
+    console.log('On éteint tout !');
+    
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appSrv.eteindreTout();
+    } else {
+      return null;
+    }
+  }
+}
