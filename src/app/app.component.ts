80e022a62e36048ecdb621136be6dcaa10f4e410
Andrew Ackah-Miezan <tawala@gmail.com>
2018-05-14 00:16:48 +0200

    appel correct observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/observable/interval';
 
 @Component({
